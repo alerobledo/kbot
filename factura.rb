6530e898ae63b91dd15260e08f46eb9ee2c09668
alerobledo <alerobledo@gmail.com>
2020-08-25 22:54:22 +0000

    Incremmento 4: crear función para calcular el impuesto (hardcoded 8.25% para CA)

--- factura.rb
+++ factura.rb
@@ -1,9 +1,15 @@
-
+class Factura
+    def calcularImpuesto()
+        "0.825".to_f
+    end
+end
 cant = ARGV[0].to_i
 precioUnitario = ARGV[1].to_f
 
 subtotal = cant*precioUnitario
 
+factura=Factura.new()
 puts "Valar Morghulis"
 puts "cant: #{cant}   -  precioUnitario: #{precioUnitario}"
 puts "subtotal: #{subtotal}"
+puts "impuesto: #{factura.calcularImpuesto()}"
