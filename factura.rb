7874674832fe1b05f2ae1994cfb3db6a2a793874
alerobledo <alerobledo@gmail.com>
2020-08-25 22:59:41 +0000

    Incremmento 5: obtener el total sumando el impuesto

--- factura.rb
+++ factura.rb
@@ -1,15 +1,22 @@
 class Factura
-    def calcularImpuesto()
-        "0.825".to_f
+    def calcularPorcentajeDeImpuesto()
+        "0.0825".to_f
     end
 end
 cant = ARGV[0].to_i
 precioUnitario = ARGV[1].to_f
 
-subtotal = cant*precioUnitario
-
 factura=Factura.new()
+
+subtotal = cant*precioUnitario
+impuesto = subtotal*factura.calcularPorcentajeDeImpuesto()
+totalBruto= subtotal+impuesto
+
+
 puts "Valar Morghulis"
 puts "cant: #{cant}   -  precioUnitario: #{precioUnitario}"
 puts "subtotal: #{subtotal}"
-puts "impuesto: #{factura.calcularImpuesto()}"
+puts "Porcentaje De Impuesto: #{factura.calcularPorcentajeDeImpuesto()}"
+puts "impuesto: #{impuesto}"
+puts "totalBruto: #{totalBruto}"
+
