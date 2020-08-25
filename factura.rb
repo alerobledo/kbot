805d0153e3d93f5852a7fe076d0f86e6dad91f3f
alerobledo <alerobledo@gmail.com>
2020-08-25 22:41:56 +0000

    Incremmento 3: determinar precio total (bruto)

--- factura.rb
+++ factura.rb
@@ -1,5 +1,9 @@
 
-cant = ARGV[0]
-precioUnitario = ARGV[1]
+cant = ARGV[0].to_i
+precioUnitario = ARGV[1].to_f
+
+subtotal = cant*precioUnitario
+
 puts "Valar Morghulis"
 puts "cant: #{cant}   -  precioUnitario: #{precioUnitario}"
+puts "subtotal: #{subtotal}"
